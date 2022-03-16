073e3f0de81d12436dcb17111d18bbf06a87f6d3
Artur Sharifullin <artursh62@gmail.com>
2022-03-16 08:24:27 +0500

    commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let burger = document.querySelector('.header__bottom-burger');
+let mainMenu = document.querySelector('.main-menu');
+let headerTop = document.querySelector('.header__top');
+burger.addEventListener('click', function () {
+	burger.classList.toggle('_active');
+	mainMenu.classList.toggle('_active');
+	headerTop.classList.toggle('_active');
+});
